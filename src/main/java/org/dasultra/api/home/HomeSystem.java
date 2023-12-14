1068ccf258d16865c102197834459aa6c0a367a4
Lukas <Lukasbaehr01@gmail.com>
2023-12-14 21:38:02 +0100

    add homesystem and tpa (base)

---
+++ src/main/java/org/dasultra/api/home/HomeSystem.java
@@ -0,0 +1,77 @@
+package org.dasultra.api.home;
+
+/*
+
+Lukas - 21:32
+14.12.2023
+https://github.com/NightDev701
+
+© SunLightScorpion 2020 - 2023
+
+*/
+
+import org.bukkit.Location;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+public class HomeSystem {
+
+    UUID uuid;
+
+    public HomeSystem(UUID uuid){
+        this.uuid = uuid;
+    }
+
+    public boolean hasHomes() {
+        return !getHomes().isEmpty();
+    }
+
+    public int getMaxHomes() {
+        return 7;
+    }
+
+    public List<String> getHomes() {
+        File file = new File("plugins/ServerAPI/playerdata/" + uuid.toString() + ".yml");
+        YamlConfiguration data = YamlConfiguration.loadConfiguration(file);
+        return data.getStringList("homes");
+    }
+
+    public Location getHome(String name) {
+        File file = new File("plugins/ServerAPI/playerdata/" + uuid.toString() + ".yml");
+        YamlConfiguration data = YamlConfiguration.loadConfiguration(file);
+        return data.getLocation("home-" + name);
+    }
+
+    public void addHome(String home) {
+        File file = new File("plugins/ServerAPI/playerdata/" + uuid.toString() + ".yml");
+        YamlConfiguration data = YamlConfiguration.loadConfiguration(file);
+        List<String> list = getHomes();
+        if (!list.contains(home)) {
+            list.add(home);
+        }
+        data.set("homes", list);
+        try {
+            data.save(file);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public void removeHome(String home) {
+        File file = new File("plugins/ServerAPI/playerdata/" + uuid.toString() + ".yml");
+        YamlConfiguration data = YamlConfiguration.loadConfiguration(file);
+        List<String> list = getHomes();
+        list.remove(home);
+        data.set("homes", list);
+        try {
+            data.save(file);
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+}
