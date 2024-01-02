c3b4e6741b3e8a4952e3014678123d8d8e3728f9
TeamUltra / DasUltra <78445891+OpUlTrA3032@users.noreply.github.com>
2024-01-02 12:35:50 +0100

    Restarter Fix

--- src/main/java/org/dasultra/listener/RestartListener.java
+++ src/main/java/org/dasultra/listener/RestartListener.java
@@ -2,6 +2,7 @@ package org.dasultra.listener;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Sound;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -14,31 +15,42 @@ import static org.dasultra.api.messages.Messages.getMessage;
 
 public class RestartListener implements Listener {
 
-    int restartTime = 5;
+    int restartTime = 10;
 
     @EventHandler
     public void onCommand(PlayerCommandPreprocessEvent event) {
         if (event.getMessage().equalsIgnoreCase("/restart") || event
                 .getMessage().equalsIgnoreCase("/stop") || event
                 .getMessage().equalsIgnoreCase("/reload") || event
-                .getMessage().equalsIgnoreCase("minecraft:restart") || event
-                .getMessage().equalsIgnoreCase("minecraft:stop") || event
-                .getMessage().equalsIgnoreCase("minecraft:reload") || event
-                .getMessage().equalsIgnoreCase("minecraft:rl") || event
-                .getMessage().equalsIgnoreCase("rl")) {
+                .getMessage().equalsIgnoreCase("/minecraft:restart") || event
+                .getMessage().equalsIgnoreCase("/minecraft:stop") || event
+                .getMessage().equalsIgnoreCase("/minecraft:reload") || event
+                .getMessage().equalsIgnoreCase("/minecraft:rl") || event
+                .getMessage().equalsIgnoreCase("/rl")) {
             if (event.getPlayer().hasPermission("core.restart")) {
                 event.setCancelled(true);
                 Bukkit.getServer().getScheduler().scheduleSyncRepeatingTask(Main.plugin, new Runnable() {
                     @Override
                     public void run() {
                         Bukkit.broadcastMessage(getMessage("Commands.Restart").replaceAll("%time%", String.valueOf(restartTime)));
-                        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
-                            onlinePlayer.sendTitle(getMessage("Commands.Restart").replaceAll("%time%", String.valueOf(restartTime)), "");
+                        if (restartTime < 6) {
+                            for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+                                onlinePlayer.sendTitle(getMessage("Commands.Restart").replaceAll("%time%", String.valueOf(restartTime)), "");
+                            }
+                        }
+
+
+
+                        if (restartTime < 4) {
+                            playSoundToAllPlayers(Sound.BLOCK_NOTE_BLOCK_HARP);
                         }
 
                         restartTime--;
 
-                        if (restartTime < 0) {
+
+
+
+                        if (restartTime < 1) {
                             Bukkit.shutdown();
                         }
                     }
@@ -50,4 +62,9 @@ public class RestartListener implements Listener {
 
     }
 
+    private void playSoundToAllPlayers (Sound sound){
+        for (Player onlinePlayer : Bukkit.getOnlinePlayers()) {
+            onlinePlayer.playSound(onlinePlayer.getLocation(), sound, 1.0f, 1.0f);
+        }
+    }
 }
